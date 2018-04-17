b27a2d2f53e1f6107ca2a63535f8b1dd9036b7da
jblockley <37510724+jblockley@users.noreply.github.com>
2018-04-17 08:56:43 +0100

    Create DayInTheLife.java

---
+++ src/main/DayInTheLife.java
@@ -0,0 +1,17 @@
+public class DayInTheLife {
+
+	public static void main(String[] args) {
+		
+		// This is where we describe a day in the life of someone.
+    // We can run this in our IDE. The "main" method is just a special method that our programming language knows what to do with.
+		// 1. We should define our person first really.
+		
+		
+		// 2. We should make our person do some stuff.
+		
+		
+		// 3. We should output useful information about the events of the day.
+		System.out.println("Nothing useful yet."); // Java specific printing of a line to the "console"
+	}
+
+}
